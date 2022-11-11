8f68f051c6ff2a86c1cd3069e470eb49239d8da4
Daniel Paralescu <dapa8486@colorado.edu>
2022-11-10 17:39:58 -0800

    Assignment 3 1 Completed

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,10 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <syslog.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -17,7 +23,22 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    /**
+     * Requirement: call system passing the parameter cmd
+    */
+    int return_value = system(cmd);
+    if (return_value != 0)
+    {
+        fprintf(stderr, "ERROR: The command %s exited with the following message, error %d\n", cmd, errno);
+        syslog(LOG_ERR, "The command %s exited with the following message, error %d\n", cmd, errno);
+        return false;
+    }
+    else
+    {
+        printf("SUCCESS: The command %s was executed successfully.\n", cmd);
+        syslog(LOG_DEBUG, "The command %s was executed successfully.\n", cmd);
+        return true;
+    }
 }
 
 /**
@@ -58,10 +79,55 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
     va_end(args);
+    /**
+    * Requirement: Execute a system command by calling fork, execv(), and wait instead of system (see LSP page 161).
+    */
+    pid_t process_id;
+    int status;
 
-    return true;
+    process_id = fork();
+    if (process_id < 0)
+    {
+        fprintf(stderr, "ERROR: Failed to create child process, error %d\n", errno);
+        syslog(LOG_ERR, "ERROR: Failed to create child process, error %d\n", errno);
+        return -1;
+    } 
+    else if (process_id == 0)
+    {
+        pid_t child_process_id;
+        child_process_id = getpid();
+        printf("SUCCESS: The child process %d is still running.\n", child_process_id);
+        syslog(LOG_DEBUG, "SUCCESS: The child process %d is still running.\n", child_process_id);
+        execv(command[0], command);
+        printf("MESSAGE: replaced existing process with  %s \n", command[0]);
+        exit(EXIT_FAILURE);
+        return false;
+    }
+    else do
+    {
+        if (waitpid(process_id, &status, 0) == -1)
+        {
+            fprintf(stderr, "ERROR: wait error, error %d\n", errno);
+            syslog(LOG_ERR, "ERROR: wait error, error %d\n", errno);
+        }
+        else if (WIFEXITED(status))
+        {
+            printf("SUCCESS: The child process exited normally %d . \n", WEXITSTATUS(status));
+            syslog(LOG_DEBUG, "SUCCESS: The child process exited normally %d . \n", WEXITSTATUS(status));
+            if (WEXITSTATUS(status) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                false;
+            }
+        }     
+
+    } while (process_id == 0);
+    
+    return false;
 }
 
 /**
@@ -92,8 +158,54 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int file_descriptor = open(outputfile, O_WRONLY | O_CREAT, 0600);
+    pid_t process_id;
+    int status;
 
-    va_end(args);
+    process_id = fork();
+    if (process_id < 0)
+    {
+        fprintf(stderr, "ERROR: Failed to create child process, error %d\n", errno);
+        syslog(LOG_ERR, "ERROR: Failed to create child process, error %d\n", errno);
+        return -1;
+    } 
+    else if (process_id == 0)
+    {
+        pid_t child_process_id;
+        child_process_id = getpid();
+        printf("SUCCESS: The child process %d is still running.\n", child_process_id);
+        syslog(LOG_DEBUG, "SUCCESS: The child process %d is still running.\n", child_process_id);
+        if (dup2(file_descriptor, 1) < 0)
+        {
+            return false;
+        }
+        execv(command[0], command);
+        printf("MESSAGE: replaced existing process with  %s \n", command[0]);
+        exit(EXIT_FAILURE);
+        close(file_descriptor);
+    }
+    else do
+    {
+        if (waitpid(process_id, &status, 0) == -1)
+        {
+            fprintf(stderr, "ERROR: wait error, error %d\n", errno);
+            syslog(LOG_ERR, "ERROR: wait error, error %d\n", errno);
+        }
+        else if (WIFEXITED(status))
+        {
+            printf("SUCCESS: The child process exited normally %d . \n", WEXITSTATUS(status));
+            syslog(LOG_DEBUG, "SUCCESS: The child process exited normally %d . \n", WEXITSTATUS(status));
+            if (WEXITSTATUS(status) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }     
 
-    return true;
+    } while (process_id == 0);
+    close(file_descriptor);
+    return false;
 }
